a8adfc10bbef37e513e860ba8419a7046a64602b
Yngve Høiseth <yhoiseth@gmail.com>
2017-04-14 12:38:06 +0200

    Fix Behat configuration

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,11 +3,12 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class FeatureContext extends MinkContext implements Context
 {
     /**
      * Initializes context.

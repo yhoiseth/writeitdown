07b40c0cf0b28a74e865390225427b202287e234
Yngve Høiseth <yhoiseth@gmail.com>
2017-04-15 07:33:09 +0200

    Fix reserved name issue

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,7 +8,7 @@ use FOS\UserBundle\Model\User as BaseUser;
 /**
  * User
  *
- * @ORM\Table(name="user")
+ * @ORM\Table(name="`user`")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
 class User extends BaseUser

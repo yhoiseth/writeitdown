f91e9c2db1f7f2738c70cb3f226f854d49bbd18e
Yngve Høiseth <yhoiseth@gmail.com>
2017-04-15 07:18:40 +0200

    Add Behat's Symfony extension

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,12 +5,15 @@ use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+use Behat\Symfony2Extension\Context\KernelDictionary;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends MinkContext implements Context
 {
+    use KernelDictionary;
+
     /**
      * Initializes context.
      *

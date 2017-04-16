d5c0937013f08902cc0de9178d4dd4fe79f69ceb
Yngve Høiseth <yhoiseth@gmail.com>
2017-04-16 16:46:09 +0200

    Fix errors

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -12,5 +12,5 @@ $container->setParameter('secret', getenv('SECRET'));
 $container->setParameter('locale', 'en');
 $container->setParameter('mailer_transport', null);
 $container->setParameter('mailer_host', null);
-$container->setParameter('mailer_user', null);
+$container->setParameter('mailer_user', 'hi@writeitdown.co');
 $container->setParameter('mailer_password', null);

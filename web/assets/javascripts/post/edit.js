3ef88b08fd74dfd7b1b2bd5572a05448ea0bf2be
Yngve Høiseth <yhoiseth@gmail.com>
2017-05-01 15:52:44 +0200

    Fix alert styling changes in response to errors

--- web/assets/javascripts/post/edit.js
+++ web/assets/javascripts/post/edit.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
   var savePost = function () {
     var $autosaveAlert = $('#autosave-alert');
-
     var $form = $('form[name="appbundle_post"]');
-
     var serializedForm = $form.serialize();
-
     var url = $form.data('autosave-url');
+    var infoClassName = 'alert-info';
+    var dangerClassName = 'alert-danger';
 
     var jqxhr = $.post(url, serializedForm, function(response) {
       $autosaveAlert.html('Your changes are saved automatically every 10 seconds.');
-      $autosaveAlert.addClass('alert-info');
+      $autosaveAlert.addClass(infoClassName);
+      $autosaveAlert.removeClass(dangerClassName);
     })
       .fail(function() {
         $autosaveAlert.html('Cannot connect to server. Your changes are not saved automatically. Check your internet connection or contact us for help.');
-        $autosaveAlert.addClass('alert-danger');
+        $autosaveAlert.addClass(dangerClassName);
+        $autosaveAlert.removeClass(infoClassName);
       })
   };
 
